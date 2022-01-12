f960b107d602c526cdfa6561067dc95928d47255
Rzk <1769073060@qq.com>
2022-01-13 02:14:17 +0800

    first :检验微信公众号服务器签名

---
+++ src/main/java/com/rzk/controller/WxServerController.java
@@ -0,0 +1,51 @@
+package com.rzk.controller;
+
+
+import com.rzk.util.SignUtil;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+@RestController
+@RequestMapping("/wx/")
+public class WxServerController {
+
+    private Logger logger = LoggerFactory.getLogger(WxServerController.class);
+
+    @Resource
+    private SignUtil signUtil;
+    @Resource
+    private Environment environment;
+
+    @RequestMapping("validate")
+    public String validate(String signature, String timestamp, String nonce, String echostr) {
+
+        logger.info("参数{}:"+signature+"  "+timestamp+"  "+ nonce+"  "+echostr);
+        //排序
+        String sort = signUtil.sort(
+                environment.getProperty("wx.Token"),
+                timestamp,
+                nonce);
+        //加密
+        String sha1 = signUtil.sha1(sort);
+
+        //检验签名
+        if (sha1 != null && sha1 != "" && sha1.equals(signature)){
+            logger.info("检验签名成功：{}"+sha1);
+            return timestamp;
+        }else{
+            logger.info("检验签名失败：{}");
+            return null;
+        }
+    }
+
+
+}
---
+++ src/main/java/com/rzk/util/SignUtil.java
@@ -0,0 +1,59 @@
+package com.rzk.util;
+
+import org.springframework.context.annotation.Configuration;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+@Configuration
+public class SignUtil {
+    /**
+     * 排序方法
+     *
+     * @param token
+     * @param timestamp
+     * @param nonce
+     * @return
+     */
+    public String sort(String token, String timestamp, String nonce) {
+        String[] strArray = {token, timestamp, nonce};
+        Arrays.sort(strArray);
+        StringBuilder sb = new StringBuilder();
+        for (String str : strArray) {
+            sb.append(str);
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * 将字符串进行sha1加密
+     *
+     * @param str 需要加密的字符串
+     * @return 加密后的内容
+     */
+    public String sha1(String str) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+            digest.update(str.getBytes());
+            byte messageDigest[] = digest.digest();
+            // Create Hex String
+            StringBuffer hexString = new StringBuffer();
+            // 字节数组转换为 十六进制 数
+            for (int i = 0; i < messageDigest.length; i++) {
+                String shaHex = Integer.toHexString(messageDigest[i] & 0xFF);
+                if (shaHex.length() < 2) {
+                    hexString.append(0);
+                }
+                hexString.append(shaHex);
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
+
